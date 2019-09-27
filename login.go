9cc200019ae1019f255e616ba36f01856d53ce36
mirandagab <gabrielmircosta@hotmail.com>
2019-09-27 13:50:24 -0300

    Finalizações
    Correções e ajustes finais

--- login.go
+++ login.go
@@ -1,82 +1,47 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
 
-	//"os"
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/events"
-	"encoding/json"
+	"github.com/aws/aws-lambda-go/lambda"
 )
-
-type token struct {
-	Token string `json:"token"`
-}
 
 func main() {
 	lambda.Start(checkLoginUnico)
-
-	//http.HandleFunc("/testego", checkLoginUnico)
-	//http.ListenAndServe(":8822", nil)
 }
 
-//func checkLoginUnico(ctx context.Context, writer http.ResponseWriter, req *http.Request) {
-func checkLoginUnico(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	//var loginUnicoURL string = os.Getenv("LoginUnicoURL")
-	//log.Println(loginUnicoURL)
-	//fmt.Println(loginUnicoURL)
-	var loginUnicoURL string = "http://loginunico-dev.sa-east-1.elasticbeanstalk.com/loginunico/token"
+func checkLoginUnico(ctx context.Context, req events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var loginUnicoURL string = os.Getenv("LoginUnicoURL")
 
-	client := &http.Client{
-		CheckRedirect: nil,
-	}
+	client := &http.Client{}
+
+	response, err := client.Get(loginUnicoURL)
+	if err != nil {
+		log.Println("Erro client.Get: " + err.Error())
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: http.StatusInternalServerError}, err
+		}
 
 	request, err := http.NewRequest("GET", loginUnicoURL, nil)
 	if err != nil {
-		//writer.WriteHeader(http.StatusUnauthorized)
-		log.Println(err)
-		//return
-		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 401}, nil
-	}
-	//localhost
-	//decoder := json.NewDecoder(req.Body)
-	//decoder := json.NewDecoder(req.Body)
-	//var token string = req.Header.Get("Authorization")
-	token := token{Token: ""}
+		log.Println("Erro http.NewRequest: " + err.Error())
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: http.StatusInternalServerError}, err
+		}
+	log.Println("headers: " + req.Headers["Authorization"])
+	request.Header.Add("Authorization", req.Headers["Authorization"])
 
-	//lambda
-	err = json.Unmarshal([]byte(req.Body), &token)
+	response, err = client.Do(request)
 	if err != nil {
-		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 401}, nil
-	}
-
-	//err = decoder.Decode(&token)
-	var tokenString string = token.Token
-	request.Header.Add("Authorization", "Bearer "+tokenString)
-
-	response, err := client.Do(request)
-	if err != nil {
-		//writer.WriteHeader(http.StatusUnauthorized)
-		log.Println(err)
-		//return
-		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 401}, nil
-	}
+		log.Println("Erro client.Do " + err.Error())
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: http.StatusInternalServerError}, err
+		}
 	defer response.Body.Close()
 	if response.StatusCode == http.StatusOK {
-		//var redirectURL string = os.Getenv("redirectSucessURL")
-		//log.Println(redirectURL)
-		//fmt.Println(redirectURL)
-		var redirectURL string = "https://onwidy7zdj.execute-api.us-east-1.amazonaws.com/default/Bucsan"
-		var writer http.ResponseWriter
-		//http.Redirect(writer, req, redirectURL, http.StatusSeeOther)
-		http.Redirect(writer, request, redirectURL, http.StatusSeeOther)
-		log.Println("token: " + tokenString)
-		return events.APIGatewayProxyResponse{Body: "", StatusCode: 200}, nil
-	}// else {
-		log.Println("login falhou com o token: " + tokenString)
-		//writer.WriteHeader(http.StatusUnauthorized)
-		//return
-		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 401}, nil
-	//}
+		return events.APIGatewayProxyResponse{Body: "ok", StatusCode: http.StatusOK}, nil
+	} 
+	log.Println("loginUnico falhou com a Authorization: " + req.Headers["Authorization"])
+	return events.APIGatewayProxyResponse{Body: "unauthorized", StatusCode: http.StatusUnauthorized}, nil
 }
